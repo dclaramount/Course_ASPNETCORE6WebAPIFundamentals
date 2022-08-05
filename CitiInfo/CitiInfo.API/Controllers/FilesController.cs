44df6af1399a3616be9267a5253679eda616b53e
dclaramount <diegoaclaramount@gmail.com>
2022-08-05 11:19:56 +0200

    File Manipulation over API

---
+++ CitiInfo/CitiInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CitiInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider ??
+                throw new System.ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            // look up the actual file, depending on the filedId..
+            //demo code
+            var pathToFile = "getting-started-with-rest-slides.pdf";
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (! _fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+            return (File(bytes, contentType, Path.GetFileName(pathToFile)));
+        }
+    }
+}
+
--- CitiInfo/CitiInfo.API/Program.cs
+++ CitiInfo/CitiInfo.API/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.StaticFiles;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -10,6 +12,7 @@ builder.Services.AddControllers(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
